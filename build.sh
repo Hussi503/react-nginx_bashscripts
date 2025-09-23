685e4672af54ab1b883db2f2449adaf92e1f203b
Hussi503 <86091094+Hussi503@users.noreply.github.com>
2025-09-23 21:00:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ cd gold/
 sudo git clone https://github.com/Hussi503/Gold_Site_Ecommerce.git
 cd Gold_Site_Ecommerce/
 sudo docker build -t react-nginx-ansible -f golddockerfile .
-sudo docker tag react-nginx-ansible:latest hussain503/react-nginx-ansible:latest ##make sure you did docker login
-sudo docker push hussain503/react-nginx-ansible:latest
+sudo docker tag react-nginx-ansible:latest hussi503/sagar-k-practice:latest ##make sure you did docker login
+sudo docker push hussi503/sagar-k-practice:latest
 
 
 ##recommended script###
