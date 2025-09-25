232ecf521fa1eb00cfd625270bb981aa7a2be213
Hussi503 <86091094+Hussi503@users.noreply.github.com>
2025-09-25 15:47:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,37 @@
-
 #!/bin/bash
-sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am deleting existing images to save space
-sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
-sudo mkdir gold
+# Delete existing images only if present
+if [ "$(sudo docker images -q)" ]; then
+  sudo docker rmi -f $(sudo docker images -q)
+fi
+
+# Clean working directory
+sudo rm -rf gold
+mkdir gold
 cd gold/
-sudo git clone https://github.com/Hussi503/Gold_Site_Ecommerce.git
+
+# Clone repo
+git clone https://github.com/Hussi503/Gold_Site_Ecommerce.git
 cd Gold_Site_Ecommerce/
+
+# Build image
 sudo docker build -t react-nginx-ansible -f golddockerfile .
-sudo docker tag react-nginx-ansible:latest hussi503/sagar-k-practice:latest ##make sure you did docker login
+
+# Tag & push (make sure docker login is done before running)
+sudo docker tag react-nginx-ansible:latest hussi503/sagar-k-practice:latest
 sudo docker push hussi503/sagar-k-practice:latest
+
+
+
+
+#sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am deleting existing images to save space
+#sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
+#sudo mkdir gold
+#cd gold/
+#sudo git clone https://github.com/Hussi503/Gold_Site_Ecommerce.git
+#cd Gold_Site_Ecommerce/
+#sudo docker build -t react-nginx-ansible -f golddockerfile .
+#sudo docker tag react-nginx-ansible:latest hussi503/sagar-k-practice:latest ##make sure you did docker login
+#sudo docker push hussi503/sagar-k-practice:latest
 
 
 ##recommended script###
